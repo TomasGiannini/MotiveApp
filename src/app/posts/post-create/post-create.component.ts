2b1280dc8b7141e3cb303baf7bfd527a3de570e2
TomasGiannini <tomasjgiannini@gmail.com>
2022-03-01 17:04:36 -0500

    test

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,73 +1,72 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormControl, FormControlName, FormGroup, FormsModule, NgForm, Validators } from '@angular/forms';
-import { PostsService } from '../posts.service';
-import { ActivatedRoute, ParamMap } from '@angular/router';
-import { Post } from '../post.model';
-import { mimeType } from './mime-type.validator';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { ActivatedRoute, ParamMap } from "@angular/router";
 
+import { PostsService } from "../posts.service";
+import { Post } from "../post.model";
+import { mimeType } from "./mime-type.validator";
 
 @Component({
-  selector: 'app-post-create',
-  templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  selector: "app-post-create",
+  templateUrl: "./post-create.component.html",
+  styleUrls: ["./post-create.component.css"]
 })
 export class PostCreateComponent implements OnInit {
-
-  enteredContent = '';
-  enteredTitle = '';
-
+  enteredTitle = "";
+  enteredContent = "";
+  post: Post;
+  isLoading = false;
   form: FormGroup;
   imagePreview: string;
-  post: Post;
-
-  private mode = 'create';
+  private mode = "create";
   private postId: string;
 
-  constructor(public postsService: PostsService, public route: ActivatedRoute) { }
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-      // defining what elements are in the create Post form
-      this.form = new FormGroup({
-        'title': new FormControl(null, {validators: [Validators.required]}),
-        'content': new FormControl(null, {validators: [Validators.required]}),
-        'image': new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]})
-      });
-
-      this.route.paramMap.subscribe((paramMap: ParamMap) => {
-        // check if there is a postId in URL, indicating if in 'Create' or 'Edit' mode
-        if (paramMap.has('postId')) {
-          this.mode = 'edit';
-          this.postId = paramMap.get('postId');
-          this.postsService.getPost(this.postId).subscribe(postData => {
-            this.post = {
-              id: postData._id,
-              title: postData.title,
-              content: postData.content,
-              imagePath: null
-            };
-            this.form.setValue({
-              'title': this.post.title,
-              'content': this.post.content
-            });
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      content: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType]
+      })
+    });
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has("postId")) {
+        this.mode = "edit";
+        this.postId = paramMap.get("postId");
+        this.isLoading = true;
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.isLoading = false;
+          this.post = {
+            id: postData._id,
+            title: postData.title,
+            content: postData.content,
+            imagePath: postData.imagePath
+          };
+          this.form.setValue({
+            title: this.post.title,
+            content: this.post.content,
+            image: this.post.imagePath
           });
-        }
-        else {
-          this.mode = 'create';
-          this.postId = null;
-        }
-      });
+        });
+      } else {
+        this.mode = "create";
+        this.postId = null;
+      }
+    });
   }
 
   onImagePicked(event: Event) {
-    // save the image file chosen
     const file = (event.target as HTMLInputElement).files[0];
-    // storing file object
-    this.form.patchValue({image: file});
-    // informs angular that value changed and it should check the validity of new value
-    this.form.get('image').updateValueAndValidity();
-
-    // convert image to data URL
+    this.form.patchValue({ image: file });
+    this.form.get("image").updateValueAndValidity();
     const reader = new FileReader();
     reader.onload = () => {
       this.imagePreview = reader.result as string;
@@ -76,26 +75,24 @@ export class PostCreateComponent implements OnInit {
   }
 
   onSavePost() {
-    if(this.form.invalid) {
+    if (this.form.invalid) {
       return;
     }
-
-    if (this.mode == 'create') {
+    this.isLoading = true;
+    if (this.mode === "create") {
       this.postsService.addPost(
         this.form.value.title,
         this.form.value.content,
         this.form.value.image
       );
-    }
-    else {
+    } else {
       this.postsService.updatePost(
         this.postId,
         this.form.value.title,
-        this.form.value.content
+        this.form.value.content,
+        this.form.value.image
       );
     }
-
     this.form.reset();
   }
-
 }
