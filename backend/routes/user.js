a48ec1630fa8590f4fa6357c6069687056370cf2
TomasGiannini <tomasjgiannini@gmail.com>
2022-03-07 11:28:52 -0500

    signup function created

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
+const User = require('../models/user');
 
 router.post("/signup", (req, res, next) => {
-
+  // bcrypt function to hash shit. Second param refers to how safe the encryption will be (longer to load tho for higher #s)
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+        .then(result => {
+          res.status(201).json({
+            message: 'User created',
+            result: result
+          });
+        })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
+    });
 });
 
 module.exports = router;
---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router, RouterModule, Routes } from "@angular/router";
+import { AuthData } from "./auth.data.model";
+
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  createUser(email: string, password: string) {
+    // user created with the auth.data.model
+    const authData: AuthData = {
+      email: email,
+      password: password
+    }
+    // the model created is sent to backend 'post' hook
+    this.http.post('http://localhost:3000/api/user/signup', authData)
+      .subscribe(response => {
+        console.log(response);
+      })
+  }
+
+}
