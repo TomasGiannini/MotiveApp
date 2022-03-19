1e9cb39f057cde9f140d81085c4d2bde941e3009
AndyCYM <yche924@uwo.ca>
2022-03-19 02:01:30 -0400

    Revert "cosmetic-v2"
    This reverts commit 9945f15e51a4b065dfae2506ce0ddd36812d3cc6.

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-post-list',
-  templateUrl: './post-list.component1.html',
+  templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit, OnDestroy {
@@ -27,37 +27,19 @@ export class PostListComponent implements OnInit, OnDestroy {
   constructor(public postsService: PostsService, private authService: AuthService) { }
 
   ngOnInit() {
-    // 使用mock
-    this.posts = ['4ttt-1646255362488.jpg',
-      '123-1646763514921.jpg', '123-1646765434138.jpg',
-      '123-1646765763244.png', '123-1646766155169.jpg', 'tomas22-1646172529005.jpg',
-      '345-1646765470942.jpg', 'ffff-1646762233912.jpg', 'rrr-1646760741962.jpg',
-      'vfvdv-1646170173122.jpg', 'ttt-1646675511647.jpg', 'vvvvvvvvv-1646855681860.jpg'
-    ].map((tt, i) => {
-      return {
-        id: 'id_' + i,
-        title: 'images_' + i,
-        content: 'images_images images_images_images_images images_images images images_images images_',
-        imagePath: '/assets/images/' + tt,
-        creator: '' + i,
-      }
-    })
-    this.totalPosts = this.posts.length;
-    this.calcGroup(this.posts);
-    /* 
+
     this.postsService.getPosts(this.postsPerPage, this.currentPage);
     this.userId = this.authService.getUserId();
-    */
 
     // subscribes to update listener
     this.postsSub = this.postsService
       .getPostUpdateListener()
-      .subscribe((postData: { posts: Post[], postCount: number }) => {
+      .subscribe((postData: {posts: Post[], postCount: number}) => {
         this.totalPosts = postData.postCount;
         this.posts = postData.posts;
       });
 
-    this.userIsAuthenticated = this.authService.getIsAuth();
+      this.userIsAuthenticated = this.authService.getIsAuth();
 
     // check authentication status of the user
     this.authStatusSub = this.authService
@@ -82,34 +64,8 @@ export class PostListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.postsSub.unsubscribe();
-    this.authStatusSub.unsubscribe();
-  }
-
-  /* 计算瀑布流图片分组 */
-  // 每列宽度400px
-  postGroup: { height: number, list: Post[] }[] = [
-    { height: 0, list: [] },
-    { height: 0, list: [] },
-    { height: 0, list: [] },
-  ];
-  calcGroup(postList: Post[]) {
-    postList.forEach(tt => {
-      let img = new Image();
-      img.onload = () => {
-        let h = 400 / img.width * img.height;// 等比例计算高度
-        h += tt.title.length * 25;// 标题加权
-        h += tt.content.length * 20;// 内容加权
-        let minHItem = this.postGroup[0];
-        this.postGroup.forEach(p => {
-          if (p.height < minHItem.height) minHItem = p;
-        });
-        minHItem.height += h;
-        minHItem.list.push(tt);
-        // console.log(img.width, img.height, h)
-      }
-      img.src = tt.imagePath;
-    });
+      this.postsSub.unsubscribe();
+      this.authStatusSub.unsubscribe();
   }
 
 }
