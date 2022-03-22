dee90e4962b7a836e1780851eea5281fe933f229
TomasGiannini <tomasjgiannini@gmail.com>
2022-03-22 12:28:33 -0400

    added chips

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -11,9 +11,37 @@ import {MatChipsModule} from '@angular/material/chips';
 })
 export class ProfilePageComponent implements OnInit {
 
+  currentlySelected = 1;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  headingClicked(value: number) {
+
+    if(value === 1) {
+      //show your events
+      this.currentlySelected = 1;
+    }
+
+    else if (value === 2) {
+      //show upcoming events
+      this.currentlySelected = 2;
+
+    }
+
+    else if (value === 3) {
+      //show past events
+      this.currentlySelected = 3;
+
+    }
+
+    else {
+      //show your events
+      this.currentlySelected = 1;
+
+    }
   }
 
 }
